3a1703a5ba578527a0ae7135a9cbb98232fff91e
Felipe Cristo de Oliveira <felipe.cristo@hotmail.com>
2017-11-12 15:16:36 -0200

    Atualização de Template e Criação de Tela de Exemplo

---
+++ Sgiq.Web/Models/ParteInteressada.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sgiq.Web.Models
+{
+    public class ParteInteressada
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
